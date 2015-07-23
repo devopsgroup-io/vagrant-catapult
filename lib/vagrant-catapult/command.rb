9eba193c57398b4a02a180a78ad0479b2d8b9966
Seth Reeser <seth.reeser@calciumusa.com>
2015-07-23 08:43:13 -0400

    Being a bit more informational.

--- lib/vagrant-catapult/command.rb
+++ lib/vagrant-catapult/command.rb
@@ -2,7 +2,7 @@ module Vagrant
     module Catapult
         class Command < Vagrant.plugin('2', :command)
             def execute
-                exec('VBoxManage list vms')
+                puts "Catapult is under development, please visit https://github.com/devopsgroup-io/catapult-release-management for more information."
                 0
             end
         end
